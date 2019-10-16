052951cc5b42a371a0937907c8e6a28835186e71
Ken Ding <ken.ding@rea-group.com>
2019-10-16 16:09:45 +1100

    Wrote getFiles
    - For next time:
    - Having errors when run
    - Figure out why we use liftIO
    - Try to substitute a different scope to see

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
-    ) where
+  where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Lens ((.~), (<&>))
+import Data.Text (Text)
+import Network.Google (newLogger, LogLevel(Debug), runResourceT, runGoogle, send)
+import Network.Google.Drive (driveReadOnlyScope, filesList)
+import Network.Google.Env (envScopes, newEnv, envLogger)
+import System.IO (stdout)
+import Control.Monad.IO.Class (liftIO)
+
+getFiles :: IO ()
+getFiles = do
+  lgr <- newLogger Debug stdout
+  env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ driveReadOnlyScope)
+  runResourceT . runGoogle env $
+    send (filesList) >>= liftIO . print
