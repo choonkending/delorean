001aa118e9f68bc71df153c66664ced755ba1b58
Ken Ding <aenkil@gmail.com>
2019-10-18 21:33:21 +1100

    Don't understand the types yet

--- src/Drive.hs
+++ src/Drive.hs
@@ -7,12 +7,12 @@ import Network.Google (newLogger, LogLevel(Debug), runResourceT, runGoogle, send
 import Network.Google.Drive (driveReadOnlyScope, filesList)
 import Network.Google.Env (envScopes, newEnv, envLogger)
 import System.IO (stdout)
-import Control.Monad.IO.Class (liftIO)
 
 -- This reads your credentials from ~/.config/gcloud/application_default_credentials.json
 getFiles :: IO ()
 getFiles = do
   lgr <- newLogger Debug stdout
   env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ driveReadOnlyScope)
-  runResourceT . runGoogle env $
-    send filesList >>= liftIO . print
+  runResourceT (runGoogle env $ send filesList)
+
+
